908283999c58e14370e7f900ca2173c169ee5ea9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 11:50:04 +0300

    Made some updates for the component

--- screens/ShoppingCartScreen.jsx
+++ screens/ShoppingCartScreen.jsx
@@ -1,5 +1,9 @@
+import { Text, StyleSheet } from 'react-native'
+
 const ShoppingCartScreen = () => {
-	return <div>ShoppingCartScreen</div>
+	return <Text>ShoppingCartScreen</Text>
 }
 
 export default ShoppingCartScreen
+
+const styles = StyleSheet.create({})
