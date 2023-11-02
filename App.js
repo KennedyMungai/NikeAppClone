92f7de106d78dc99a49c7646cc2e336655d97f6b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 09:20:48 +0300

    Set up the FlatList to render the image of each individual nike shoe

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, FlatList, View, Image } from 'react-native';
 import products from './data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' }}
-        style={styles.imageItem}
+      <FlatList
+        data={products}
+        renderItem={({ item }) =>
+        (
+          <Image
+            source={{ uri: item.image }}
+            style={styles.imageItem}
+          />
+        )
+        }
       />
+
       <StatusBar style="auto" />
     </View>
   );
