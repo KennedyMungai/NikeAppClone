6a1adfd1f7f43f453e2f18ee87947a2dd0507713
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 09:15:14 +0300

    Deleted the text from the main page and imported the products data

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import products from './data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <Image
         source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' }}
         style={styles.imageItem}
