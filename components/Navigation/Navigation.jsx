755215b2d5677d29b3dd6a7d08bb55bca338e3fc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 14:49:50 +0300

    Created and implemented stack navigation

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native'
 import ProductsScreen from '../../screens/ProductsScreen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 const Navigation = () => {
 	return (
 		<NavigationContainer>
-			<ProductsScreen />
+			<Stack.Navigator>
+				<Stack.Screen name='Products' component={ProductsScreen} />
+			</Stack.Navigator>
 		</NavigationContainer>
 	)
 }
