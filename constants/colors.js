9c224b21c7580dc4e509da4dde030941bfeee4d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 10:37:56 +0300

    Refactored the object otems

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,7 @@
 export default {
-    "eerie-black": "#191716",
-    "black-bean": "#440D0F",
-    "eggPlant": "#603A40",
-    "mauveTaupe": "#84596B",
-    "glossyGrape": "#AF9BB6"
+    eerieBlack: "#191716",
+    blackBean: "#440D0F",
+    eggPlant: "#603A40",
+    mauveTaupe: "#84596B",
+    glossyGrape: "#AF9BB6"
 }
