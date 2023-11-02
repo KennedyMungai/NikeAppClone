74a5c080f0e02e4510d4335abe4cf18e01b735b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 09:14:07 +0300

    Made a bunch of playful changes

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' }}
+        style={styles.imageItem}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageItem: {
+    width: "100%",
+    aspectRatio: 1
+  }
 });
