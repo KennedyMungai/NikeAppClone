339c997e5b085fdaf3382fa16b01a86192077543
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 11:48:50 +0300

    Rendered out the shopping cart screen

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ProductsScreen from './screens/ProductsScreen';
 import ProductDetailScreen from './screens/ProductDetailScreen';
+import ShoppingCartScreen from './screens/ShoppingCartScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ProductsScreen /> */}
-      <ProductDetailScreen />
+      {/* <ProductDetailScreen /> */}
+      <ShoppingCartScreen />
       <StatusBar style="auto" />
     </View>
   );
