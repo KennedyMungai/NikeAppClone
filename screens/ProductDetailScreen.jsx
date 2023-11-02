4432f594a1a93f62764874e9bdd59d18671768a1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-02 09:42:52 +0300

    Created the ProductDetailScreen for the application

---
+++ screens/ProductDetailScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native'
+import products from '../data/products'
+
+const ProductDetailScreen = () => {
+	const product = products[0]
+	console.log(product)
+
+	return <View>ProductDetailScreen</View>
+}
+
+export default ProductDetailScreen
+
+const styles = StyleSheet.create({})
